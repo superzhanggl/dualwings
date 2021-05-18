221966957fe7a8a74bef7aa864601e711196bfdb
superzhanggl <514547062@qq.com>
2021-05-19 00:14:37 +0800

    1

--- com-dualwings-user/src/test/java/user/ApplicationTest.java
+++ com-dualwings-user/src/test/java/user/ApplicationTest.java
@@ -13,7 +13,6 @@ public class ApplicationTest {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationTest.class,args);
-        System.out.println("ApplicationTest*************");
     }
 
 }
